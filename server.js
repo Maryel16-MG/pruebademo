e6ef5d43f0b6bb68b134099aaa68004fda4f5cd3
Thyfa <thmartinezgo@est.utn.ac.cr>
2025-06-19 13:57:24 -0600

    Agrego API para retornar JSON

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/api/data', (req, res) => {
+  res.json({ mensaje: "Hola desde Azure" });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
